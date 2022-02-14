88e8297a9a7f7ff0911cce41caa1c02f0c087cc7
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-02-14 15:53:25 +0200

    added shared preferences

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'package:notes/models/note.dart';
+import 'package:notes/screens/create_note.dart';
 import 'package:notes/urls.dart';
 
 class MainMenu extends StatefulWidget {
@@ -35,33 +36,58 @@ class _MainMenuState extends State<MainMenu> {
     setState(() {});
   }
 
+  void _deleteNote(int id) {
+    client.delete(deleteUrl(id));
+    _retrieveNotes();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Main Menu'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListView.builder(
-                itemCount: notes.length,
-                itemBuilder: (BuildContext context, index) {
-                  return ListTile(
-                      title: Text(
-                    notes[index].note,
-                  ));
-                })
-          ],
-        ),
-      ),
+      body: RefreshIndicator(
+          onRefresh: () async {
+            _retrieveNotes();
+          },
+          child: ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                color: Colors.grey[9],
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.white30, width: 9),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: const EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: ListTile(
+                    title: Text('Note - ' + notes[index].id.toString()),
+                    subtitle: Text(notes[index].note),
+                    onTap: () {
+                      // Navigator.of(context).push(MaterialPageRoute(
+                      //     builder: (context) => UpdatePage(
+                      //           client: client,
+                      //           note: notes[index].note,
+                      //           id: notes[index].id,
+                      //         )));
+                    },
+                    trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _deleteNote(notes[index].id);
+                        }),
+                  ),
+                ),
+              );
+            },
+          )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _addnote;
-        },
+        onPressed: () =>
+            MaterialPageRoute(builder: (context) => const CreateNote()),
         tooltip: 'Add Note',
-        child: const Icon(Icons.add),
       ),
     );
   }
